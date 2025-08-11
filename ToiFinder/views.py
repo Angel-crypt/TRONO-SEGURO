86345d15451fa8432c6dc0b4b81d142057cfa61c
Crypt <72517634+Angel-crypt@users.noreply.github.com>
2025-08-11 00:28:06 -0600

    feat: implement chat query functionality with ChromaDB integration for bathroom recommendations

--- ToiFinder/views.py
+++ ToiFinder/views.py
@@ -5,6 +5,11 @@ from django.shortcuts import render
 from django.http import HttpResponseBadRequest, JsonResponse
 from django.views import View
 
+import chromadb
+from sentence_transformers import SentenceTransformer
+from django.conf import settings
+from ToiFinder.models import Bathroom
+
 from .models import User, Location, Bathroom, Review
 
 # Create your views here.
@@ -34,3 +39,32 @@ class PaginaView(View):
     def get(self, request):
         baños = Bathroom.objects.all().values()
         return render(request, 'ToiFinder/index.html', {'baños': baños})
+
+
+@csrf_exempt
+def chat_query(request):
+    if request.method == 'POST':
+        query = request.POST.get('query', '')
+
+        # Carga ChromaDB y modelo
+        client = chromadb.PersistentClient(path=str(settings.CHROMA_DB_PATH))
+        collection = client.get_collection(name="bathrooms")
+        model = SentenceTransformer('paraphrase-multilingual-MiniLM-L12-v2')
+
+        # Embed la query y busca top 3 resultados
+        query_embedding = model.encode(query)
+        results = collection.query(query_embeddings=[query_embedding], n_results=3)
+
+        # Formatea respuesta
+        response = "\n"
+        for i, (doc, metadata) in enumerate(zip(results['documents'][0], results['metadatas'][0])):
+            bath_id = metadata['bathroom_id']
+            bath = Bathroom.objects.get(id=bath_id)
+            response += f"Baño recomendado: {metadata['location']} - {doc}\n"
+
+        return JsonResponse({'response': response})
+    elif request.method == 'GET':
+        # Renderiza la template del chat
+        return render(request, 'ToiFinder/chat.html')
+
+    return JsonResponse({'error': 'Método no permitido'}, status=405)
